25d942656bbb59133899198e3396cd578818f8bf
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-11-06 00:16:31 +0200

    Implement function for getting path to resource

--- src/filesystem/filesystem.py
+++ src/filesystem/filesystem.py
@@ -1,3 +1,7 @@
+import os.path
+from src.webserver.webserver import State
+
+
 class Filesystem:
     def __init__(self, root: str, default_page: str, server_state: State, maintenance_page: str,
                  resource_not_found_page: str) -> str:
@@ -8,4 +12,14 @@ class Filesystem:
         self.resource_not_found_page = resource_not_found_page
 
     def get_resource_path(self, resource):
-        raise NotImplementedError()
+        path_to_resource = os.path.join(self.root, resource)
+        if self.server_state == State.RUNNING:
+            return self.__get_resource_path_server_running(path_to_resource)
+        elif self.server_state == State.MAINTENANCE:
+            return self.maintenance_page
+
+    def __get_resource_path_server_running(self, path_to_resource) -> str:
+        if os.path.isfile(path_to_resource):
+            return path_to_resource
+        else:
+            return self.resource_not_found_page
