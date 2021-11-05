4803eb7b415397bb62241684a2736f3ae4b51f68
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-11-05 23:43:48 +0200

    Use type hints for parameters and return value

--- src/filesystem/filesystem.py
+++ src/filesystem/filesystem.py
@@ -1,5 +1,6 @@
 class Filesystem:
-    def __init__(self, root, default_page, server_state, maintenance_page, resource_not_found_page):
+    def __init__(self, root, default_page: str, server_state: str, maintenance_page: str,
+                 resource_not_found_page: str) -> str:
         self.root = root
         self.default_page = default_page
         self.server_state = server_state
