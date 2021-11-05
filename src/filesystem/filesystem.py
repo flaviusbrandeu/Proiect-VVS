7a57b03c19d17bf39c60c7868be86149d9bc28a6
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-11-05 18:43:28 +0200

    Use a new class for getting requested resources

---
+++ src/filesystem/filesystem.py
@@ -0,0 +1,10 @@
+class Filesystem:
+    def __init__(self, root, default_page, server_state, maintenance_page, resource_not_found_page):
+        self.root = root
+        self.default_page = default_page
+        self.server_state = server_state
+        self.maintenance_page = maintenance_page
+        self.resource_not_found_page = resource_not_found_page
+
+    def get_resource_absolute_path(self, resource):
+        raise NotImplementedError()
--- src/webserver/connection.py
+++ src/webserver/connection.py
@@ -23,9 +23,6 @@ class Connection:
         # TODO: implement __get_requested_resource_path
         pass
 
-    def __get_resource_content(self, path: str) -> str:
-        # TODO: implement __get_resource_content
-        pass
     def __send_response(self):
         # TODO: implement __send_response
         pass
