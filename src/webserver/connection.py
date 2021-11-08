612bf87c965e5deb5e5168178206cbd09eed2415
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-11-08 14:16:54 +0200

    Implement handling request functionality

--- src/webserver/connection.py
+++ src/webserver/connection.py
@@ -18,16 +18,40 @@ class Connection:
         self.maintenance_page_path = maintenance_page_path
 
     def handle_request(self):
-        raise NotImplementedError()
+        request = self.socket_connection.recv(MAX_REQUEST_LENGTH)
+        requested_resource = self.__extract_requested_resource(request)
+        filesystem = Filesystem(self.root_directory, self.default_page_path, self.server_state,
+                                self.maintenance_page_path, self.resource_not_found_page_path)
+        resource_path = filesystem.get_resource_path(requested_resource)
+        print(resource_path)
+        try:
+            with open(resource_path, "rb") as resource:
+                content = resource.read()
+        except OSError:
+            resource_path = self.resource_not_found_page_path
+            with open(resource_path, "rb") as resource:
+                content = resource.read()
+        response_header = self.__get_response_header(resource_path)
+        try:
+            self.socket_connection.sendall(response_header)
+            self.socket_connection.sendall(content)
+            self.socket_connection.sendall(b"\r\n\r\n")
+        except socket.error:
+            print("[ERROR] Sending data failed")
+        finally:
+            self.socket_connection.close()
 
-    def __get_request_header(self) -> str:
-        # TODO: implement __get_request_header
-        pass
+    @staticmethod
+    def __extract_requested_resource(data: bytes) -> str:
+        request_line_bytes, _ = data.split(b"\r\n", 1)
+        requests_line = request_line_bytes.decode("ISO-8859-1")
+        return urllib.parse.unquote((requests_line.split(" ")[1]))
 
-    def __extract_requested_resource(self, header: str) -> str:
-        # TODO: implement __get_requested_resource
-        pass
-
-    def __send_response(self):
-        # TODO: implement __send_response
-        pass
+    def __get_response_header(self, resource_path: str) -> bytes:
+        if self.server_state == State.RUNNING:
+            if resource_path == self.resource_not_found_page_path:
+                return b"HTTP/1.1 404 Not Found\r\n\r\n"
+            else:
+                return b"HTTP/1.1 200 OK\r\n\r\n"
+        elif self.server_state == State.MAINTENANCE:
+            return b"HTTP/1.1 503 Service Unavailable\r\n\r\n"
