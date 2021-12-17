71d0c498d5e5a022fb605214f11475826a3687d1
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-12-17 20:02:16 +0200

    Change index file for using the test site

--- src/demo_main.py
+++ src/demo_main.py
@@ -4,7 +4,7 @@ from src.webserver.webserver import Webserver
 # noinspection SpellCheckingInspection
 def main():
     webserver = Webserver(8081, '/mnt/nvme0n1p6/Facultate/VVS/laborator/TestSite/',
-                          '/mnt/nvme0n1p6/Facultate/VVS/laborator/TestSite/index.html',
+                          '/mnt/nvme0n1p6/Facultate/VVS/laborator/TestSite/a.html',
                           '/mnt/nvme0n1p6/Facultate/VVS/laborator/TestSite/404.html',
                           '/mnt/nvme0n1p6/Facultate/VVS/laborator/TestSite/Maintenance.html')
     webserver.start()
